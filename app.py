7f933c8ed6843b970c96694ae9c2a3fbc181147f
Mike <vmichele80@hotmail.com>
2026-03-18 23:50:02 +0100

    Created the first version of app.py

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
-
 from flask import Flask
+from data_manager import DataManager
+from models import db, Movie
+import os
 
 app = Flask(__name__)
 
 
 
-"""
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
-);
+basedir = os.path.abspath(os.path.dirname(__file__))
+# this creates the data folder if it does not exist
+os.makedirs(os.path.join(basedir, "data"), exist_ok=True)
 
-"""
+# if data folder does not exist, the db creation will fail
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-"""""
+db.init_app(app)  # Link the database and the app. This is the reason you need to import db from models
 
-CREATE TABLE movies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    director TEXT NOT NULL,
-    year INTEGER,
-    poster_url TEXT
-    user_id INTEGER FOREIGN KEY
-);
-    
-"""""
+data_manager = DataManager() # Create an object of your DataManager class
+
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+if __name__ == '__main__':
+  with app.app_context():
+    db.create_all()
+
+  app.run()
