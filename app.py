b67ee6d63b7f9aef0e9c3224a242e0ee876bf97d
Mike <vmichele80@hotmail.com>
2026-03-22 13:45:58 +0100

    Changed the port to make it compatible with Codio

--- app.py
+++ app.py
@@ -207,4 +207,5 @@ def internal_server_error(e):
     return render_template("500.html"), 500
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
